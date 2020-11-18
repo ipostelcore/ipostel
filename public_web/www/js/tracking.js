89a1fd4cdfa5efbc8f907d122b13cee075bc0fe1
unknown <maikolechenique.com>
2020-11-18 10:07:05 -0400

    creacion de clases

--- public_web/www/js/tracking.js
+++ public_web/www/js/tracking.js
@@ -126,33 +126,7 @@ class Distribucion{
     }
 
 }
-    class Distribucion{
-        constructor(){
-            this.hora = "";
-            this.fecha = "";
-            this.sticker = "";
-            this.destinatario = "";
-            this.remitente = "";
-            this.peso = "";
-            this.estatus = "";
-            this.observacion = "";
     
-        }
-        Crear(req){}
-        Obtener(){
-        this.hora = $("#txtHora").val();
-        this.fecha = $("#txtFecha").val();
-        this.sticker = $("#txtSticker").val();
-        this.destinatario = $("#txtDestinatario").val();
-        this.remitente = $("#txtRemitente").val();
-        this.peso = $("#txtPeso").val();
-        this.tipodistribucion = $("#cmbTipoDistibucion option:selected").val();
-        this.observacion = $("#txtObservacion").val();
-        
-    
-        return this;
-    }
-}
 class Ruta{
     constructor(){
         this.localidad = "";
